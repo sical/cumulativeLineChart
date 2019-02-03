0df2f4ca253dfee01f1ae2b98b06a0363f453031
Mohamed Ezzaouia <mohamed@speakplus.fr>
2019-02-03 17:30:01 +0100

    --amedn

--- chart-line/src/configureStore.js
+++ chart-line/src/configureStore.js
@@ -1,15 +1,15 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+// import { composeWithDevTools } from 'redux-devtools-extension'
 
 import logger from './utils/logger.store'
 import { rootReducer } from './reducers'
 
 const configureStore = preloadedState => {
   const enhancers = compose( applyMiddleware( thunk, logger ))
-  const composedEnhancers = composeWithDevTools( enhancers )
+  // const composedEnhancers = composeWithDevTools( enhancers )
 
-  const store = createStore( rootReducer, preloadedState, composedEnhancers )
+  const store = createStore( rootReducer, preloadedState, enhancers )
 
   if ( process.env.NODE_ENV !== 'production' && module.hot ) {
     module.hot.accept( './reducers', _ => store.replaceReducer( rootReducer ))
